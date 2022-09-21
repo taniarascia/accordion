7de3e5bbb0bfebf22ebc2b55cd684cd90380155d
Tania Rascia <hello@taniarascia.com>
2022-09-20 21:01:09 -0500

    Chromatic accordion

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Router, Route } from 'svelte-routing'
+
+  import Home from './home.svelte'
+  import Diatonic from './diatonic.svelte'
+  import Chromatic from './chromatic.svelte'
+
+  export let url = ''
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="chromatic" component={Chromatic} />
+    <Route path="diatonic" component={Diatonic} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
